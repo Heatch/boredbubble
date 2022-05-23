c9d65dda58a8ce532f79546dd34bdd38611d795f
heatch <hiteshguptacad@gmail.com>
2022-05-23 16:18:22 -0400

    cleaner code

--- docs/mainjs.js
+++ docs/mainjs.js
@@ -57,6 +57,7 @@ function buildTable(data) {
                     }
                 }
                 else {
+                    circleGen(getCircles());
                     // show all rows
                     // remove the "hide-row" class from all rows.
                     for (let i = 0; i < tableRows.length; i++) {
@@ -134,9 +135,83 @@ function buildTable(data) {
             //excluding circle generation from the top of the table
 
                 elementDef = document.getElementById('bHeader');
+                elementDef2 = document.getElementById('theBottom');
                 var topExc = elementDef.getBoundingClientRect();
+                var botExc = elementDef2.getBoundingClientRect();
 
-                possibleHeights = [];
                 topEdge = topExc.top;
                 bottomEdge = topExc.top + topExc.height;
- 
+                zebottom = botExc.top;
+
+                var selectMenu = document.querySelector(".menu");
+                selectMenu.onchange = function () {
+                    console.log("changed"); }
+
+
+            //creating circles in the background 
+
+            function getCircles() {
+
+                number = 0;
+                hiddenRows = document.getElementsByClassName("hide-row");
+                number = myArray.length - hiddenRows.length;
+                return number;
+
+            }
+
+            function circleGen(num) {
+
+            console.log("hi");
+            circles = [];
+
+            for (i = 0; i < (num * 2); i++) {
+                
+                maxHeight = $(document).height() - 250;
+
+                circles.push(document.createElement('div'));
+                circles[i].setAttribute('id',i);
+                document.body.appendChild(circles[i]);
+
+                document.getElementById(i).style.borderRadius = '50%';
+                document.getElementById(i).style.zIndex = -1;
+
+                randomColor = Math.floor(Math.random()*16777215).toString(16);
+                randomColor = "#" + randomColor;
+                document.getElementById(i).style.backgroundColor = randomColor;
+
+                randomSize = Math.floor(Math.random() * (150 - 30 + 1) + 30).toString();
+                randomSize = randomSize + "px";
+                document.getElementById(i).style.height = randomSize;
+                document.getElementById(i).style.width = randomSize;
+
+                randomP1 = Math.floor(Math.random() * (maxHeight - 1 + 1) + 1).toString();
+                if (((randomP1 >= topEdge) && (randomP1 <= bottomEdge))) {
+                    while (((randomP1 >= topEdge) && (randomP1 <= bottomEdge)) || (randomP1 >= zebottom)) {
+                        randomP1 = Math.floor(Math.random() * (maxHeight - 1 + 1) + 1).toString();
+                    }
+                }
+                if (randomP1 >= document.getElementById('theBottom').getBoundingClientRect().top) {
+                    while (randomP1 >= document.getElementById('theBottom').getBoundingClientRect().top) {
+                        randomP1 = Math.floor(Math.random() * (maxHeight - 1 + 1) + 1).toString();
+                    }
+                } 
+                randomP1 = randomP1 + 'px';
+
+                randomP2 = Math.floor(Math.random() * (100 - 1 + 1) + 1).toString();
+                randomP2 = randomP2 + '%';
+                document.getElementById(i).style.position = "absolute";
+                document.getElementById(i).style.top = randomP1;
+                document.getElementById(i).style.right = randomP2; 
+
+                randomOp = Math.floor(Math.random() * (100 - 1 + 1) + 1).toString();
+                randomOp = randomOp + "%";
+                document.getElementById(i).style.opacity = randomOp;
+
+               }
+            }
+
+       $(window).on('load', function(){ 
+            
+               circleGen(getCircles());
+
+           }); 
